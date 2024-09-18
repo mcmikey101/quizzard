d64efccc56a9d00c45fb46b4aa1fbdf625e1bcfd
mcmikey <kamil.rahmatullin@gmail.com>
2024-09-18 15:23:50 +0300

    more things

---
+++ front/src/comps/UserPage.tsx
@@ -0,0 +1,15 @@
+import './styles/userpage.css'
+
+export default function UserPage() {
+    return (
+        <>
+            <div className="userpage">
+                <div className="backcont">
+                    <button className="back">Back</button>
+                </div>
+                <div className="sections"></div>
+                <div className="useroutlet"></div>
+            </div>
+        </>
+    )
+}
